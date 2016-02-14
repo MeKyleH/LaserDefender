2a9ee1d16df2149bfd8aa3688f06d54e3f474aa9
meKyleh <kyle.a.holt@gmail.com>
2016-02-13 20:31:55 -0700

    fixed menu music bug

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -8,9 +8,11 @@ public class MusicPlayer : MonoBehaviour {
 	public AudioClip gameClip;
 	public AudioClip endClip;
 
+	private bool startClipPlaying = true;
 	private AudioSource music;
 
 	void Start () {
+
 		if (instance != null && instance != this) {
 			Destroy (gameObject);
 			print ("Duplicate music player self-destructing!");
@@ -21,24 +23,27 @@ public class MusicPlayer : MonoBehaviour {
 			music.clip = startClip;
 			music.loop = true;
 			music.Play ();
+			Debug.Log ("Music Player Start else block: " + startClip); 
 		}
-		
 	}
 
 	void OnLevelWasLoaded(int level) {
-		Debug.Log ("MusicPlayer loaded level" + level);
-
+		Debug.Log ("MusicPlayer loaded level " + level);
+		if (level != 1 && level != 2 && startClipPlaying) {
+			return;
+			Debug.Log ("Start music is playing, no change");
+		}
 		music.Stop ();
 
 		if (level == 0) {
 			music.clip = startClip;
-		}
-		if (level == 1) {
+			startClipPlaying = true;
+		} else if (level == 1) {
 			music.clip = gameClip;
-		}
-		if (level == 2) {
+			startClipPlaying = false;
+		} else if (level == 2) {
 			music.clip = endClip;
-		}
+		} 
 		music.loop = true;
 		music.Play ();
 	}
